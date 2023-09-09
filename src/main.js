6c435b569e402810b4d14ba33c53debaf1f40d0c
buck1105 <baclx@tasc.vn>
2023-09-09 18:28:19 +0700

    update src

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import VueApexCharts from "vue3-apexcharts";
+import PerfectScrollbar from "vue3-perfect-scrollbar";
+import "vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css";
+import { createPinia } from "pinia";
+// import "flowbite";
+import "./assets/tailwind.css";
+import "./assets/animate.css";
+import "./assets/sass/css/windzo.css";
+import vClickOutside from "click-outside-vue3";
 
-loadFonts()
-
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(createPinia());
+app.use(VueApexCharts);
+app.use(PerfectScrollbar);
+app.use(vClickOutside);
+app.mount("#app");
